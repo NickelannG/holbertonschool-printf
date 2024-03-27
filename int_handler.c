1b8b5c54733e3a2f75afd106884616651a0068c5
WennieL <109007964+WennieL@users.noreply.github.com>
2024-03-27 12:00:07 +1100

    Update int_handler.c

--- int_handler.c
+++ int_handler.c
@@ -21,7 +21,7 @@
 int int_handler(va_list args)
 {
         int num, count = 0;
-        int div = 1;
+        int div;
 
         num = va_arg(args, int);
 
@@ -35,20 +35,22 @@ int int_handler(va_list args)
         if (num == 0)
         {
                 _putchar('0');
-                return (1);
-        }
-
-        while (num / div >= 10)
-        {
-                div *= 10;
-        }
-
-        while (div != 0)
-        {
-                _putchar((num / div) + '0');
                 count++;
-                num %= div;
-                div /= 10;
+        }
+        else
+        {
+                div = 1;
+                while (num / div >= 10)
+                {
+                        div *= 10;
+                }
+                while (div != 0)
+                {
+                        _putchar((num / div) + '0');
+                        count++;
+                        num %= div;
+                        div /= 10;
+                }
         }
         return (count);
 }
