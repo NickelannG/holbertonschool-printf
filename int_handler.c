47a3c82c16fc72d0c64a93f8384453a51f6b91cd
WennieL <109007964+WennieL@users.noreply.github.com>
2024-03-29 17:26:47 +1100

    change method in int_handler.c

--- int_handler.c
+++ int_handler.c
@@ -1,34 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <stdarg.h>
 #include "main.h"
-#include <limits.h>
 
-/**
- * int_neg - a recursive function that prints negative integers
- * char by char
- *
- * @num: num to print
- *
- * Return:
- * count of digits printed
- */
-
-int int_neg(int num)
-{
-	int count = 1;
-	int r = -(num % 10);
-	char c = r + '0';
-
-	if (num < -9)
-	{
-		count = count + int_neg((num + r) / 10);
-	}
-	write(1, &c, 1);
-
-	return (count);
-}
 /**
  * int_handler - Handles the '%d' conversion specifier.
  * @args: A va_list containing the variable arguments.
@@ -47,36 +21,31 @@ int int_neg(int num)
 
 int int_handler(va_list args)
 {
-	int num, count = 0;
-	int div;
+        unsigned int n, div;
+        int count = 0, num;
 
-	num = va_arg(args, int);
+        num = va_arg(args, int);
+        if (num < 0)
+        {
+                n = (num * -1);
+                count+= _putchar('-');
+                _putchar(n);
+        }
+        else
+                n = num;
 
-	if (num < 0)
-	{
-		_putchar('-');
-		count = int_neg(num) + 1;
-	}
-
-	else if (num == 0)
-	{
-		_putchar('0');
-		count++;
-	}
-	else
-	{
-		div = 1;
-		while (num / div >= 10)
-		{
-			div *= 10;
-		}
-		while (div != 0)
-		{
-			_putchar((num / div) + '0');
-			count++;
-			num %= div;
-			div /= 10;
-		}
-	}
-	return (count);
+        div = 1;
+        while (n / div > 9)
+        {
+                div *= 10;
+        }
+        while (div >= 1)
+        {
+                    _putchar((n / div) + '0');
+                    count++;
+                    n %= div;
+                    div /= 10;
+        }
+        return (count);
 }
+   
